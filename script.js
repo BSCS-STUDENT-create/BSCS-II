49281c779c66be5cedafec29023f0b6efe7310a9
BSCS II <kingrheo116@gmail.com>
2025-12-22 10:06:20 +0800

    Update script.js

--- script.js
+++ script.js
@@ -7,25 +7,25 @@ function addJudge() {
   const newInput = document.createElement("input");
   newInput.id = `s${judgeCount}`;
   newInput.type = "number";
-  newInput.placeholder = `Judge ${judgeCount} Score`;
+  newInput.placeholder = `J${judgeCount} Score`; // Shorter placeholder to save space
   container.appendChild(newInput);
 }
 
 function addContestant() {
-  const name = document.getElementById("name").value;
-  if (!name) return alert("Please enter a name");
+  const nameInput = document.getElementById("name");
+  const name = nameInput.value;
+  if (!name) return alert("Enter a name!");
 
   let total = 0;
   for (let i = 1; i <= judgeCount; i++) {
-    const score = Number(document.getElementById(`s${i}`).value || 0);
-    total += score;
+    total += Number(document.getElementById(`s${i}`).value || 0);
   }
 
   const average = (total / judgeCount).toFixed(2);
   contestants.push({ name, total, average });
 
   // Clear inputs
-  document.getElementById("name").value = "";
+  nameInput.value = "";
   for (let i = 1; i <= judgeCount; i++) {
     document.getElementById(`s${i}`).value = "";
   }
@@ -34,23 +34,19 @@ function addContestant() {
 }
 
 function rankContestants() {
-  if (contestants.length === 0) return;
   contestants.sort((a, b) => b.total - a.total);
-
   const tbody = document.getElementById("tableBody");
   tbody.innerHTML = "";
 
   contestants.forEach((c, index) => {
-    tbody.innerHTML += `
-      <tr>
-        <td>${index + 1}</td>
-        <td>${c.name}</td>
-        <td>${c.total}</td>
-        <td>${c.average}</td>
-      </tr>
-    `;
+    tbody.innerHTML += `<tr>
+      <td>${index + 1}</td>
+      <td>${c.name}</td>
+      <td>${c.total}</td>
+      <td>${c.average}</td>
+    </tr>`;
   });
 
-  document.getElementById("highest").innerText = "Highest Performer: " + contestants[0].name;
-  document.getElementById("lowest").innerText = "Lowest Performer: " + contestants[contestants.length - 1].name;
+  document.getElementById("highest").innerText = "Highest: " + contestants[0].name;
+  document.getElementById("lowest").innerText = "Lowest: " + contestants[contestants.length - 1].name;
 }
